b207ee1b07e1dd52de735477bfe4113f6d9dea37
shakerscode <ahamedshaker7@gmail..com>
2025-05-01 22:37:31 +0600

    feat: added api and improved the code quality in pipeline creation page

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import { RouterProvider } from "react-router";
 import { AuthProvider } from "./hooks/AuthContext";
 import "./index.css";
 import { router } from "./routes/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
